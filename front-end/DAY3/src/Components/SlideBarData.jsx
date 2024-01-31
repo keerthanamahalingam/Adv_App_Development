186a3722d80a5552d672278e173e0803635f8157
keerthanamahalingam <117429218+keerthanamahalingam@users.noreply.github.com>
2024-01-31 10:01:32 +0530

    Add files via upload

---
+++ front-end/DAY3/src/Components/SlideBarData.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+  {
+    title: "Events",
+    path: "/events",
+ 
+    cName: "nav-text"
+  },
+  {
+    title: "Themes",
+    path: "/themes",
+   
+    cName: "nav-text"
+  },
+  {
+    title: "Venues",
+    path: "/Result",
+ 
+    cName: "nav-text"
+  },
+  {
+    title: "Bookings",
+    path: "/booking",
+ 
+    cName: "nav-text"
+  }
+];
