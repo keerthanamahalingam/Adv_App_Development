8457110dfa4309f902fae32be0d2e860f67aae3f
keerthanamahalingam <117429218+keerthanamahalingam@users.noreply.github.com>
2024-01-31 12:20:18 +0530

    Add files via upload

---
+++ front-end/DAY4/AboutUs.jsx
@@ -0,0 +1,37 @@
+
+
+import React from 'react';
+import './AboutUs.css';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-page">
+     
+      <div className="about-us-content">
+        <h2>About Us</h2>
+        <div className="about-us-text">
+          <p>
+            Welcome to our party event management, your premier partner for creating unforgettable party events.
+          </p>
+          <p>
+            At our event , we specialize in delivering exceptional experiences that
+            leave lasting memories. Our dedicated team of event planners, designers, and
+            coordinators is passionate about turning your vision into reality.
+          </p>
+          <p>
+            What sets us apart is our commitment to creativity, attention to detail, and
+            unparalleled customer service. Whether it's a birthday celebration, wedding,
+            corporate event, or any special occasion, we go above and beyond to ensure
+            every event is a resounding success.
+          </p>
+          <p>
+            Meet the creative minds behind our event management who work tirelessly to make
+            every event extraordinary.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
